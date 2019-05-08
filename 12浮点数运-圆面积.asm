45f1cdada3248739908f0532556c385c402ba552
zyjie <zhongyinjiebit@sina.com>
2019-05-08 19:30:45 +0800

    浮点数运-圆面积

--- 12浮点数运-圆面积.asm
+++ 12浮点数运-圆面积.asm
@@ -11,24 +11,26 @@ scanf PROTO C:ptr sbyte, :VARARG
 ;数据段
 .data					
 	;定义这些分段是把不同类型的数据或代码归类，放到不同的属性内存页中，dos下定义是赋值给寄存器
-fmt_out sbyte '%f', 0ah, 0
-a real8 3.2 ;定义8字节（64位）浮点数
-b real8 2.6
-m real8 7.1
-f real8 ?
+fmt_out db 's = %lf', 0ah, 0
+fmt_in db '%lf',  0
+fmt_msg db 'enter radius, please:', 0ah, 0
+radius real8 ? ;定义8字节（64位）浮点数， 半径
+s real8 ? ;面积
 
 ;代码段
 .code					;所有指令都必须放在代码段，在可执行文件中，代码段放在_TEXT节区（区块）中）
 start:
 main proc
+    invoke printf, offset fmt_msg
+    invoke scanf, offset fmt_in, offset radius
     finit ;FPU寄存器的初始化
-    fld m ;进栈
-    fld b
+    fld radius ;半径进栈
+    fld radius ;
     fmul st(0), st(1)
-    fld a
-    fadd st(0), st(1)
-    fst f; 将寄存器栈st(0)保存到目标操作数f
-    invoke printf, offset fmt_out, f
+    fldpi ;加载pi常量
+    fmul st(0), st(1)
+    fst s;保存面积
+    invoke printf, offset fmt_out, s
 main endp
 end main
 end start
