e50bf02c39acb5a82318248d93e6c042f1b4ad26
zyjie <zhongyinjiebit@sina.com>
2019-05-08 08:50:22 +0800

    阶乘

--- 07循环之单循环程序.asm
+++ 07循环之单循环程序.asm
@@ -12,17 +12,26 @@ printf PROTO C:ptr sbyte, :VARARG ;函数声明
 	;定义这些分段是把不同类型的数据或代码归类，放到不同的属性内存页中，dos下定义是赋值给寄存器
 sum dd 0
 fmt_out sbyte '1+2+3+...+100 = %d', 0ah, 0
+fmt_out1 sbyte 'fact(%d) = %d', 0ah, 0
+n equ 6
 ;代码段
 	.code					;所有指令都必须放在代码段，在可执行文件中，代码段放在_TEXT节区（区块）中）
 start:
     mov ecx, 100
     mov eax, 1
-d10:
+d10:;累加计算
     add sum, eax
     inc eax ;加一
     loop d10
-    invoke printf, offset fmt_out, sum
+	invoke printf, offset fmt_out, sum
+    
+    mov ecx, n
+    mov eax, 1 ;fact初值
+e10:;阶乘计算
+    imul eax, ecx
+    loop e10
+    invoke printf, offset fmt_out1, n, eax
     ret
 end start
 
-;1+2+3+...+100,循环次数确定
+;1+2+3+...+100、n!,循环次数确定
