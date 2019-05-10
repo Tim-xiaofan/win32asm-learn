cb80c4e4da748a0aecc0d52799b7c3ee83e66453
zyjie <zhongyinjiebit@sina.com>
2019-05-10 13:11:31 +0800

    '添加说明'

--- 22C程序调用汇编子程序/00.c
+++ 22C程序调用汇编子程序/00.c
@@ -3,17 +3,20 @@ c模块读入x, y，输出结果
 汇编模块读入a, b, 进行计算
 */
 
-#include "stdio.h"
+#include <stdio.h>
 
 extern int a, b; //a,b来自汇编模块
 extern int calcAXBX(int x, int y);//调用汇编模块子程序
 extern int z; //可以被外部模块使用
 int z;
 
-int main(int argc, char* argv[]){
-    int x = 0, y = 0;
-    printf("enter x, y");
-    scanf("%d%d", &x, &y);
-    int r = calcAXBX(x, y);
-    printf("%d * %d + %d * %d = %d, r = %d\n", a, x, b, y, z, r);
+int main(int argc, char* argv[]) {
+	int x = 0, y = 0;
+	printf("enter x, y\n");
+	scanf("%d%d", &x, &y);
+	int r = calcAXBX(x, y);
+	printf("%d * %d + %d * %d = %d, r = %d\n", a, x, b, y, z, r);//r值恒为0
+	return 0;
 }
+
+//00.c抵用01.asm的子程序进行计算
